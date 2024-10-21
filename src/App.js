887f031b2ca60ccb561706f1cc5320a823dcfa29
sajid <iamsajidalam007@gmail.com>
2024-10-21 15:58:32 +0530

    wizardtask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import wizard from './Img/wizard.gif';
 
-function App() {
+
+
+console.log(wizard,">>>>>>>>>>")
+
+const App = () => {
+  const [position, setPosition] = useState({ top: 100, left: 100 });
+  const [facingRight, setFacingRight] = useState(true);
+
+  const handleClick = (e) => {
+    const clickX = e.clientX;
+    console.log(clickX,"clickXXXXXXXXXXXXX")
+
+    const clickY = e.clientY;
+    console.log(clickY,"clickYyyyyyyyyyy")
+    
+    setPosition({ top: clickY, left: clickX });
+
+    setFacingRight(clickX > position.left); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" onClick={handleClick}>
+      <img
+        src={wizard}
+        alt="wizard"
+        className={`wizard ${facingRight ? 'right' : 'left'}`}
+        style={{ top: `${position.top}px`, left: `${position.left}px` }}
+      />
     </div>
   );
-}
+};
 
 export default App;
